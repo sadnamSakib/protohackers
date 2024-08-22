4ad05c70e06e896adf051c352b5447a321404a54
Sadnam Sakib Apurbo <sadnamsakib@iut-dhaka.edu>
2024-08-22 17:52:09 +0600

    added concurrent message handling

--- budgetchat/budgetchat.go
+++ budgetchat/budgetchat.go
@@ -7,84 +7,105 @@ import (
 	"strings"
 )
 
-func nameResolution(conn *net.Conn, connections map[string]*net.Conn) string {
-	buf := make([]byte, 1024)
-	n, err := (*conn).Read(buf)
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-	clientName := string(buf[:n])
-	if len(clientName) == 0 {
-		(*conn).Write([]byte("Invalid name."))
-		(*conn).Close()
-		return ""
-	}
-	clientName, _ = strings.CutSuffix(clientName, "\n")
-	fmt.Println("Client : ", clientName)
-	pattern := `^[[:alnum:]]{1,16}$`
-	r := regexp.MustCompile(pattern)
-	if !r.MatchString(clientName) {
-		(*conn).Write([]byte("Invalid name."))
-		(*conn).Close()
-		return ""
-	} else {
-		otherMemebers := make([]string, 0, len(connections))
-		for key := range connections {
-			otherMemebers = append(otherMemebers, key)
-		}
-
-		roomMembersMessage := "* The room contains: " + strings.Join(otherMemebers, ", ") + "\n"
-		for _, roomMembers := range connections {
-			(*roomMembers).Write([]byte(fmt.Sprintf("* %s has entered the room\n", clientName)))
-		}
-		fmt.Printf("Server: * %v has entered the room\n", clientName)
-		connections[clientName] = conn
-		(*conn).Write([]byte(roomMembersMessage))
-		return clientName
-	}
-
+type Client struct {
+	name string
+	conn net.Conn
 }
 
-func handleRequest(conn *net.Conn, connections map[string]*net.Conn) {
-	serverMessage := "Welcome to budgetchat! What shall I call you?\n"
-	(*conn).Write([]byte(serverMessage))
-	fmt.Println("Server : ", serverMessage)
-	name := nameResolution(conn, connections)
+func nameResolution(conn net.Conn, connections map[string]Client) (string, bool) {
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return "", false
+	}
+	clientName := string(buf[:n])
+	clientName = strings.TrimSpace(clientName)
+
+	if len(clientName) == 0 || !isValidName(clientName) {
+		conn.Write([]byte("Invalid name.\n"))
+		conn.Close()
+		return "", false
+	}
+
+	otherMembers := make([]string, 0, len(connections))
+	for key := range connections {
+		otherMembers = append(otherMembers, key)
+	}
+
+	roomMembersMessage := "* The room contains: " + strings.Join(otherMembers, ", ") + "\n"
+	conn.Write([]byte(roomMembersMessage))
+
+	return clientName, true
+}
+
+func isValidName(name string) bool {
+	pattern := `^[[:alnum:]]{1,16}$`
+	r := regexp.MustCompile(pattern)
+	return r.MatchString(name)
+}
+
+func handleClient(client Client, join chan<- Client, leave chan<- Client, broadcast chan<- string, clients map[string]Client) {
 	defer func() {
-		delete(connections, name)
-		for _, roomMembers := range connections {
-			(*roomMembers).Write([]byte(fmt.Sprintf("* %s has left the room\n", name)))
-		}
-		fmt.Printf("Server: * %v has left the room\n", name)
-		(*conn).Close()
+		leave <- client
+		client.conn.Close()
 	}()
+
+	client.conn.Write([]byte("Welcome to budgetchat! What shall I call you?\n"))
+
+	name, ok := nameResolution(client.conn, clients)
+	if !ok {
+		return
+	}
+
+	client.name = name
+	join <- client
 
 	for {
 		buf := make([]byte, 1024)
-		n, err := (*conn).Read(buf)
+		n, err := client.conn.Read(buf)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		clientMessage := string(buf[:n])
-		clientMessage, _ = strings.CutSuffix(clientMessage, "\n")
-		clientMessage, _ = strings.CutSuffix(clientMessage, "\r")
-		clientMessage, _ = strings.CutSuffix(clientMessage, " ")
-		fmt.Println("Client: ", clientMessage)
-		fmt.Printf("[%v] %q\n", name, clientMessage)
-		if clientMessage != "" {
-			for otherClients, roomMembers := range connections {
-				if name != otherClients {
-					(*roomMembers).Write([]byte(fmt.Sprintf("[%s] %s", name, clientMessage)))
-				}
-			}
-		} else {
 
+		clientMessage := strings.TrimSpace(string(buf[:n]))
+		if clientMessage != "" {
+			broadcast <- fmt.Sprintf("[%s] %s\n", client.name, clientMessage)
+		} else {
 			break
 		}
 	}
+}
 
+func manageClients(join <-chan Client, leave <-chan Client, broadcast <-chan string, clients map[string]Client) {
+
+	for {
+		select {
+		case client := <-join:
+			clients[client.name] = client
+			message := fmt.Sprintf("* %s has entered the room\n", client.name)
+			for _, c := range clients {
+				if c.name != client.name {
+					c.conn.Write([]byte(message))
+				}
+			}
+			fmt.Print(message)
+
+		case client := <-leave:
+			delete(clients, client.name)
+			message := fmt.Sprintf("* %s has left the room\n", client.name)
+			for _, c := range clients {
+				c.conn.Write([]byte(message))
+			}
+			fmt.Print(message)
+
+		case message := <-broadcast:
+			for _, client := range clients {
+				client.conn.Write([]byte(message))
+			}
+		}
+	}
 }
 
 func Run() {
@@ -93,16 +114,24 @@ func Run() {
 		fmt.Println(err)
 		return
 	}
+	defer listen.Close()
+
 	fmt.Println("- listening on port 8000")
-	connections := make(map[string]*net.Conn)
+
+	join := make(chan Client)
+	leave := make(chan Client)
+	broadcast := make(chan string)
+	clients := make(map[string]Client)
+	go manageClients(join, leave, broadcast, clients)
+
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
 			fmt.Println(err)
-			return
+			continue
 		}
 
-		go handleRequest(&conn, connections)
+		client := Client{conn: conn}
+		go handleClient(client, join, leave, broadcast, clients)
 	}
-
 }
